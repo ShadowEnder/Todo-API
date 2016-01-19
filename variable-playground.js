7d7731d303ce63edd064d813699c34d5d0ee176d
Jonathan Randacciu <groundon777@msn.com>
2016-01-18 20:43:33 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Jonathan',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jonathan',
+// 	// 	age: 22
+// 	// }
+// 	obj.age = 22;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradeArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
