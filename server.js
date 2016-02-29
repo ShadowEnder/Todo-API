842d9578812dce4e2fbfac897688fb3630b45c13
Jonathan Randacciu <groundon777@msn.com>
2016-02-29 02:07:17 -0500

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -114,7 +114,17 @@ app.put('/todos/:id', function(req, res) {
 	});
 });
 
-db.sequelize.sync().then(function() {
+app.post('/users', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function (user) {
+		res.json(user.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+});
+
+db.sequelize.sync({logging: console.log}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
